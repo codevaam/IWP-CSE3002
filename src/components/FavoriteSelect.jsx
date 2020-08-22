51755f42b1204639f048bdfcbd13cf760ca635da
devamtrivedi <devamtrivedi71@gmail.com>
2020-08-22 23:52:42 +0530

    fix: ui fix for search bar

--- src/components/FavoriteSelect.jsx
+++ src/components/FavoriteSelect.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import styles from '../stylesheets/components/interests.module.scss';
 import Helmet from 'react-helmet';
 import Artists from '../static/test.json';
+import { render } from '@testing-library/react';
 
 
 function FavoriteSelect() {
     const [result, updateResult] = useState([]);
+    const [addedArtists, updateArtists] = useState([]);
+    const [ArtistHtml, updateArtistHtml] = useState([]);
     function searchArtist(event) {
         var name = event.target.value;
         var lowercasedFilter = name.toLowerCase();
@@ -15,9 +18,16 @@ function FavoriteSelect() {
             );
         });
         updateResult(filteredData);
-        console.log(result)
     }
+    function addArtist(event) {
+        addedArtists.push(event.target.value)
+        console.log(addedArtists);
+        updateArtists(addedArtists);
 
+        // ArtistHtml.push(<div key={event.target.value} className={`mt-4 mr-2 d-flex ${styles.circle}`}><span className={`m-auto`}>event.target.value</span></div>)
+        // updateArtistHtml(ArtistHtml)
+        // console.log(ArtistHtml)
+    }
     return (
         <div>
             <Helmet>
@@ -26,20 +36,26 @@ function FavoriteSelect() {
             <h2 className={styles.title}>ADD YOUR FAVORITE ARTISTS</h2>
             <input type="text" placeholder="SEARCH" className={styles.search} onChange={searchArtist} />
             <div className="d-flex justify-content-center w-50">
-                <div className={`mt-4 mr-2 d-flex ${styles.circle}`}><span className={`m-auto`}>JOJI</span></div>
-                <div className={`mt-4 mr-2 d-flex ${styles.circle}`}><span className={`m-auto`}>ARCTIC MONKEYS</span></div>
+                {addedArtists.map(item => (
+                    <div key={item} className={`mt-4 mr-2 d-flex ${styles.circle}`}><span className={`m-auto`}>{item}</span></div>
+                ))}
+                <div className={`mt-4 mr-2 d-flex ${styles.circle}`}><span className={`m-auto`}>ADD +</span></div>
             </div>
 
-            {result.map(item => (
-                <div key={item.email}>
-                    <div className={styles.resultDiv}>
-                        {item.Artist} {item.URI}
+            <div className="d-flex flex-column align-items-center">
+                {result.map(item => (
+                    <div key={item.URI}>
+                        <div className={`mt-4 d-flex justify-content-between ${styles.resultDiv}`}>
+                            <span>{item.Artist}</span>
+                            {/* item.URI */}
+                            <button className={`${styles.followBtn}`} value={item.Artist} onClick={addArtist}>Follow +</button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
 
             <div className="d-flex justify-content-end mr-5">
-                <button className={`align-self-right mb-4 ${styles.nextBtn}`}>NEXT</button>
+                <button className={`mb-4 ${styles.nextBtn}`}>NEXT</button>
             </div>
         </div>
     )
